513d378ef333563c3f5875b9d6e563923c79d976
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-19 02:13:33 +0900

    Import FaceDetector, and if the smilingProbability value exceeds 0.7 as the measurement value of face.smilingProbability, happy appears, otherwise, the message "Laugh" appears.
    npm install expo-face-detector ,
    onFacesDetected={smileDetected? null: onFacesDetected}
    If a smiling face is recognized, the recognition function is terminated at that moment. If not, continue face recognition.

--- Contents/Screen.js
+++ Contents/Screen.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Camera } from "expo-camera";
 import { Dimensions } from "react-native";
 import styled from "styled-components/native";
+import * as FaceDetector from "expo-face-detector";
 
 const Container = styled.View`
   margin: 10px 0px;
@@ -10,8 +11,17 @@ const Container = styled.View`
 const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 
 const Screen = ({ type, zoom, whiteBalance }) => {
-  const onFacesDetected = (faces) => {
-    console.log(faces);
+  const [smileDetected, setSmileDetected] = useState(false);
+  const onFacesDetected = ({ faces }) => {
+    const face = faces[0];
+    if (face) {
+      if (face.smilingProbability > 0.7) {
+        setSmileDetected(true);
+        console.log("happy! ^^ ");
+      } else if (face.smilingProbability < 0.6) {
+        console.log("웃어보세요!");
+      }
+    }
   };
   return (
     <Container>
@@ -23,7 +33,13 @@ const Screen = ({ type, zoom, whiteBalance }) => {
         type={type}
         zoom={zoom}
         whiteBalance={whiteBalance}
-        onFacesDetected={onFacesDetected}
+        onFacesDetected={smileDetected ? null : onFacesDetected}
+        faceDetectorSettings={{
+          mode: FaceDetector.Constants.Mode.fast,
+          detectLandmarks: FaceDetector.Constants.Landmarks.all,
+          runClassifications: FaceDetector.Constants.Classifications.all,
+          minDetectionInterval: 100,
+        }}
       />
     </Container>
   );
