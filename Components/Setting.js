88d49df1728575f5334f4d2b64965ae82c5e53cb
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-19 00:39:25 +0900

    Import components such as revert, screen, and whitebalance, and design a container using styled components.

---
+++ Components/Setting.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import { Camera } from "expo-camera";
+import styled from "styled-components/native";
+import { ActivityIndicator, Text } from "react-native";
+import Revert from "../Contents/Revert";
+import Screen from "../Contents/Screen";
+import WhiteBalance from "../Contents/WhiteBalance";
+
+const Container = styled.View`
+  justify-content: flex-start;
+  padding-top: 50px;
+  align-items: center;
+  background-color: #f67280;
+  flex: 1;
+`;
+
+const Setting = () => {
+  const [hasPermission, setHasPermission] = useState(true);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [zoom, setZoom] = useState(0);
+  const [whiteBalance, setWhiteBalance] = useState("auto");
+
+  useEffect(() => {
+    async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    };
+  }, []);
+
+  if (hasPermission === null) {
+    return (
+      <Container>
+        <ActivityIndicator size="large" color="white" />
+      </Container>
+    );
+  } else if (hasPermission === false) {
+    return (
+      <Container>
+        <Text>접근 차단</Text>
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+        <WhiteBalance setWhiteBalance={setWhiteBalance} />
+        <Screen type={type} zoom={zoom} whiteBalance={whiteBalance} />
+        <Revert type={type} setType={setType} setZoom={setZoom} />
+      </Container>
+    );
+  }
+};
+
+export default Setting;
