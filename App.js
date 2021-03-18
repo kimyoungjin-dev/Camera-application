4a40900901e04fe15335b08f58516320cf2fc1b9
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-18 20:59:25 +0900

    If haspermission is asked and given, camera access is allowed, if false, a message asking for access is displayed, and if null, loading screen is displayed.

--- App.js
+++ App.js
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Camera } from "expo-camera";
+import { ActivityIndicator, Text, View } from "react-native";
 
 const App = () => {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      if (status === "granted") {
+        setHasPermission(true);
+      } else {
+        setHasPermission(false);
+      }
+    };
+  }, []);
+
+  if (hasPermission === true) {
+    return (
+      <View>
+        <Text>접근 허용되었습니다.</Text>
+      </View>
+    );
+  } else if (hasPermission === false) {
+    return (
+      <View>
+        <Text>접근이 거절되었습니다.</Text>
+      </View>
+    );
+  } else {
+    return <ActivityIndicator />;
+  }
+
   return <></>;
 };
 
