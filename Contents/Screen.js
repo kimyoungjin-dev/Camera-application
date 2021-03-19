e27975759e063796161201baf7dc9544151aad71
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-03-19 16:30:08 +0900

    Write a more explanatory comment.

--- Contents/Screen.js
+++ Contents/Screen.js
@@ -46,19 +46,21 @@ const Screen = ({ type, zoom, whiteBalance, setHasPermission }) => {
   const savePhoto = async (uri) => {
     try {
       const { status } = await MediaLibrary.requestPermissionsAsync();
+      //status가 granted 인지 확인한다.
+
       if (status === "granted") {
-        const asset = await MediaLibrary.createAssetAsync(uri);
-        let album = await MediaLibrary.getAlbumAsync(ALBUM_NAME);
+        const asset = await MediaLibrary.createAssetAsync(uri); //asset을 만든다.
+        let album = await MediaLibrary.getAlbumAsync(ALBUM_NAME); //특정 이름을 가진 앨범을 쿼리한다.
         if (album === null) {
-          album = await MediaLibrary.createAlbumAsync(ALBUM_NAME, asset, false);
+          album = await MediaLibrary.createAlbumAsync(ALBUM_NAME, asset, false); //앨범이 존재하지않는다면 , 앨범을 생성한다
         } else {
-          await MediaLibrary.addAssetsToAlbumAsync([asset], album.id, false);
+          await MediaLibrary.addAssetsToAlbumAsync([asset], album.id, false); //앨범이있다면 ,asset을 앨범에 넣는다.
         }
         setTimeout(() => {
           setSmileDetected(false);
-        }, 2000);
+        }, 2000); //2초 간격으로 사진을찍는다.
       } else {
-        setHasPermission(false);
+        setHasPermission(false); //status==="granted"가 아니라면 접근을 하지못한다는 문구를 보여준다.
       }
     } catch (error) {
       console.log(error);
